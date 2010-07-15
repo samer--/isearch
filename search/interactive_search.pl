ff8c14f85de3aad7775e406afa6d4b82f816e28e
Michiel Hildebrand <michiel.hildebrand@cwi.nl>
2010-07-15 22:42:18 +0200

    add facets

--- search/interactive_search.pl
+++ search/interactive_search.pl
@@ -7,12 +7,21 @@
 :- use_module(library(http/http_wrapper)).
 :- use_module(library(http/http_host)).
 :- use_module(library(http/html_head)).
+:- use_module(library(http/json)).
+:- use_module(library(http/json_convert)).
 :- use_module(library(semweb/rdf_db)).
 :- use_module(library(semweb/rdfs)).
 :- use_module(library(semweb/rdf_litindex)).
 :- use_module(library(semweb/rdf_label)).
 
 :- http_handler(root(isearch), interactive_search_page, []).
+
+:- json_object
+     prop(prop:atom, values:_),
+     literal(literal:atom),
+     literal(literal:_),
+     type(type:atom, text:atom),
+     lang(lang:atom, text:atom).
 
 interactive_search_page(Request) :-
 	http_parameters(Request,
@@ -24,13 +33,21 @@ interactive_search_page(Request) :-
 			       [optional(true), description('Disambiguation term')]),
 			  related(Related,
 				  [optional(true), description('Related term')]),
+			  filter(FilterAtom,
+				 [default([]),
+				  description('Filters on the result set')]),
 			  offset(Offset,
 				[default(0), integer, description('Offset of the result list')]),
 			  limit(Limit,
 				[default(20), integer, description('Limit on the number of results')])
 			]),
+	(   atom_json_term(FilterAtom, FilterJSON, [])
+	->  json_to_prolog(FilterJSON, Filter)
+	;   Filter = []
+	),
+
 	% collect results
-	findall(Path-R, search_result(Query, Class, R, Path), PathResults),
+	findall(Path-R, search_result(Query, Class, Filter, R, Path), PathResults),
 
 	% collected terms
 	terms_in_paths(PathResults, TermResults0),
@@ -61,29 +78,42 @@ interactive_search_page(Request) :-
 	list_offset(Rs, Offset, Rs1),
 	list_limit(Rs1, Limit, Results, _),
 
+	% collect facets
+	facets(Rs, Facets),
+
 	% emit html page
 	reply_html_page([ title(['Search results for ',Query]),
 			  script(type('text/javascript'),
 				 \js_toggle)
 			],
 			[  \html_requires(css('interactive_search.css')),
-			   div(class(search),
+			   div(id(search),
 			       \search_field(Query, Class)
 			      ),
-			   div(a([id(ltoggle),href('#'),
-				  onClick('javascript:bodyToggle(\'ltoggle\',\'left\', [\'<\',\'>\']);')
+			   div([id(left), class(column)],
+			       [ div(class(toggle),
+				     a([id(ltoggle),href('#'),
+					onClick('javascript:bodyToggle(\'ltoggle\',\'lbody\', [\'<\',\'>\']);')
 				 ], '<')),
-			   div([id(left), class(left)],
-			       [ \html_term_list(MatchingTerms, Query, Class, Term),
-				 \html_related_term_list(RelatedTerms, Query, Class, Term, Related)
+				 div([class(body), id(lbody)],
+				     [ \html_term_list(MatchingTerms, Query, Class, Term),
+				       \html_related_term_list(RelatedTerms, Query, Class, Term, Related)
+				     ])
 			       ]),
-			   div(class(results),
+			   div(id(results),
 			      [ div(class(header),
 				  \html_result_header(Query, Term, Related, Class, NumberOfResults)
 				   ),
 				\html_result_list(Results)
 			      ]),
-			  div(class(paginator),
+			   div([id(right), class(column)],
+			       [ div(class(toggle), a([id(rtoggle),href('#'),
+					onClick('javascript:bodyToggle(\'rtoggle\',\'rbody\', [\'>\',\'<\']);')
+				 ], '>')),
+				 div([class(body), id(rbody)],
+				     \html_facets(Facets, Query, Class, Term, Related, Filter))
+			      ]),
+			   div(class(paginator),
 			      \html_paginator(NumberOfResults, Offset, Limit)
 			     )
   			]).
@@ -158,16 +188,44 @@ html_result_list([R|Rs]) -->
 	html_result_list(Rs).
 
 format_result(R) -->
+	html(div(class('result-item'),
+		 [ div(class(title), a(href(R), \result_title(R))),
+		   div(class(thumbnail),
+		       \result_image(R)),
+		   div(class(text),
+		       [ div(class(subtitle), \result_subtitle(R)),
+			 div(class(description), \result_description(R))
+		       ])
+		 ])).
+
+result_title(R) -->
 	{ (   rdf_label(R, Lit)
 	  ->  literal_text(Lit, Label)
 	  ;   rdfs_label(R, Label)
 	  )
 	},
-	html(div(class('result-item'),
-		 [div(class(thumbnail),
-		      \result_image(R)),
-		  a(href(R), Label)
-		 ])).
+	html(Label).
+result_subtitle(R) -->
+	{ (   rdf_has(R, dc:creator, C)
+	  ->  rdf_label(C, Lit),
+	      literal_text(Lit, Creator)
+	  ;   Creator = ''
+	  ),
+	  (   rdf_has(R, dc:date, D)
+	  ->  literal_text(D, Date)
+	  ;   Date = ''
+	  )
+	},
+	html([Creator, ' (', Date, ')']).
+result_description(R) -->
+	{ rdf_has(R, dc:description, LitDesc),
+	  literal_text(LitDesc, DescTxt),
+	  truncate_atom(DescTxt, 300, Desc)
+	},
+	!,
+	html(Desc).
+result_description(_R) --> !.
+
 
 result_image(R) -->
 	{ rdf(Image, 'http://www.vraweb.org/vracore/vracore3#relation.depicts', R)
@@ -270,13 +328,16 @@ all_link(Term, URL) -->
 	!,
 	item_link(term, URL, 'All', '').
 all_link(_, _) -->
-	html(li(class(term), 'All')).
+	html(li(class(term),
+		div(class(label),'All'))).
 
 item_link(CSSClass, Link, Label, Count) -->
 	html(li(class(CSSClass),
-		 [ a(href(Link), Label),
-		   span(class(count), Count)
-		 ])).
+		a(href(Link),
+		  [ div(class(count), Count),
+		    div(class(label), Label)
+		  ]
+		 ))).
 
 term_list([], _, _) --> !.
 term_list([H|T], Active, URL) -->
@@ -328,6 +389,77 @@ related_term_list_rest(Rest, Related, URL, N) -->
 	     ]).
 
 
+%%	html_facets(+Facets, +Query, +Class, +Term, +Related, +Filter)
+%
+%	Emit html with facet filters.
+
+html_facets(Facets, Query, Class, Term, Related, Filter) -->
+	{ http_location_by_id(interactive_search_page, URL0),
+	  url_parameters([q=Query,class=Class,term=Term,related=Related], Params),
+	  URL = URL0+'?'+Params
+	},
+	facets(Facets, Filter, URL).
+
+facets([], _, _) --> !.
+facets([P-Values|Fs], Filter, URL) -->
+	{ rdfs_label(P, Label)
+	},
+	html(div(class(facet),
+		 [ div(class(header), Label),
+		   div(class(items),
+		       ul(
+			  \facet_values(Values, P, Filter, URL))
+		      )
+		 ])),
+	facets(Fs, Filter, URL).
+
+facet_values([], _, _, _) --> !.
+facet_values([Value-Rs|T], P, Filter0, URL) -->
+	{ (   Value = literal(_)
+	  ->  literal_text(Value, Label0)
+	  ;   rdfs_label(Value, Label0)
+	  ),
+	  truncate_atom(Label0, 75, Label),
+	  length(Rs, Count),
+	  update_filter(Filter0, P, Value, Filter),
+	  prolog_to_json(Filter, FilterJSON),
+	  with_output_to(string(FilterString0),
+		       json_write(current_output, FilterJSON, [width(0)])),
+	  www_form_encode(FilterString0, FilterString),
+ 	  Link = URL+'filter='+FilterString
+	},
+	html(li(class(value),
+		a(href(Link),
+		  [ div(class(count), Count),
+		    div(class(label), Label)
+ 		  ])
+	       )),
+	facet_values(T, P, Filter, URL).
+
+update_filter([], P, Value, [Filter]) :-
+	Filter = prop(P, [Value]).
+update_filter([F|Fs], P, Value, Filters) :-
+	F = prop(P, Values),
+	!,
+	(   select(Value, Values, Rest)
+	->  (	Rest = []
+	    ->	Filters = Fs
+	    ;	Filters = [prop(P, Rest)|Fs]
+	    )
+	;   Filters = [prop(P, [Value])|Fs]
+	).
+update_filter([F|Fs], P, Value, [F|Filters]) :-
+	update_filter(Fs, P, Value, Filters).
+
+url_parameters([], '').
+url_parameters([Key=Value|T], Ps) :-
+	(   nonvar(Value)
+	->  Ps = Key+'='+Value+'&'+Rest
+	;   Ps = Rest
+	),
+	url_parameters(T, Rest).
+
+
 %%	js_toggle
 %
 %	Emit javascript function to toggle display of an element.
@@ -356,25 +488,43 @@ js_toggle -->
 %
 %	True if R is related to Query and of type Class.
 
-search_result(Query, Class, R, [P,Query]) :-
+search_result(Query, Class, Filter, R, Path) :-
+	filter_to_goal(Filter, R, FilterGoal),
+	(   nonvar(Class)
+	->  Goal = (instance_of_class(Class, R), FilterGoal)
+	;   Goal = FilterGoal
+	),
+	result(Query, Goal, R, Path).
+
+result(Query, Goal, R, [P,Query]) :-
 	once(rdf(_,_,Query)),
 	!,
 	rdf(R, P, Query),
-	instance_of_class(Class, R).
-search_result(Query, Class, R, Path) :-
+	call(Goal).
+result(Query, Goal, R, Path) :-
 	rdf_find_literals(case(Query), Literals),
 	member(Lit, Literals),
 	rdf(S, P, literal(Lit)),
-	search_path(S, P, Class, R, [P,literal(Lit)], Path).
+	search_path(S, P, Goal, R, [P,literal(Lit)], Path).
 
-search_path(S, _P, Class, S, Path, Path) :-
- 	instance_of_class(Class, S),
+search_path(S, _P, Goal, S, Path, Path) :-
+	call(Goal),
 	!.
-search_path(O, P, Class, R, Path, [Prop,O|Path]) :-
+search_path(O, P, Goal, R, Path, [Prop,O|Path]) :-
 	rdfs_subproperty_of(P, rdfs:label),
 	rdf(R, Prop, O),
-	instance_of_class(Class, R).
+	call(Goal).
 
+filter_to_goal([], _, true).
+filter_to_goal([prop(P, Values)|T], R, (Goal,Rest)) :-
+	pred_filter(Values, P, R, Goal),
+	filter_to_goal(T, R, Rest).
+
+pred_filter([Value], P, R, Goal) :- !,
+	Goal = rdf(R, P, Value).
+pred_filter([Value|Vs], P, R, Goal) :-
+	Goal =  (rdf(R, P, Value), Rest),
+	pred_filter(Vs, P, R, Rest).
 
 %%	term_result(+Term, +Class, -R)
 %
@@ -414,6 +564,28 @@ related(S, O, P) :-
 	rdf_eq(O, P, S),
 	rdf(P, owl:inverseOf, IP),
 	\+ rdf_eq(S, IP, O).
+
+
+%%	facets(+Results, -Facets)
+%
+%	Collect faceted properties of Results.
+
+facets(Results, Facets) :-
+	bagof(P-Values,
+		bagof(V-Rs,
+		      bagof(R,
+			    ( member(R, Results),
+			      resource_property(R, P, V)
+			    ),
+			    Rs
+			   ),
+		      Values
+		     ),
+		Facets).
+
+resource_property(S, P, O) :-
+	rdf(S, P, O).
+
 
 rdf_eq(S, P, O) :-
 	rdf(S, P, O).
