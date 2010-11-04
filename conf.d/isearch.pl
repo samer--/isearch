d5af26138cc62ab78dddc0576b4f89d31adce1b1
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2010-11-04 22:34:11 +0100

    Default configuration

---
+++ conf.d/isearch.pl
@@ -0,0 +1,16 @@
+:- module(conf_isearch, []).
+:- use_module(isearch(applications/isearch)).
+:- use_module(cliopatria(hooks)).
+
+/** <module> Interactive search
+
+Provide interactive search based on   property-chains and multi-facetted
+browsing.
+
+@author Michiel Hildebrand
+*/
+
+cliopatria:menu_item(250=query/search,  'Search').
+
+
+
